890e8518422d62ebbc72bb7a7e01e234dee54f1e
hn04145 <hn04145@naver.com>
2022-12-06 07:02:27 +0000

    CBV detail page commit

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 # 경로 urls.py -> index.py
 urlpatterns = [
-    path('<int:pk>/',views.single_post_page),
-    # views.single_post_page로 이동한다.
+    path('<int:pk>/',views.PostDetail.as_view()),
+    # path('<int:pk>/',views.single_post_page), 사용 안 함(PostDetail로 변경되었음)
     
-    path('', views.index),
+    # path('', views.index), Class 형태로 변환되었기 때문에 주석 처리한다.
+    path('',views.PostList.as_view()),
+    # post_list.html 형태로 만든다.
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,54 @@
-from django.shortcuts import render
+# CBV 방식으로 변경하기
+# from django.shortcuts import render
+from django.views.generic import ListView, DetailView    # Detail을 불러오겠음
 from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # Post 나열
+    # index 함수의 역할을 대신하게 된다.
+    # Blog의 urls를 전부 수정해야 한다.
+    
+class PostDetail(DetailView):
+    model = Post
+    
+
+# from django.shortcuts import render
+# from .models import Post
  
-# 함수 방식으로 만들기
-# render : '템플릿'이라고 하는 폴더를 찾으러 감
+# # 함수 방식으로 만들기
+# # render : '템플릿'이라고 하는 폴더를 찾으러 감
 
-def index(request):
+# def index(request):
     
-    # objects.all() : DB Query 명령어
-    # DB에 있는 것들을 전부 가져온다.
+#     # objects.all() : DB Query 명령어
+#     # DB에 있는 것들을 전부 가져온다.
     
-    posts = Post.objects.all().order_by('-pk')
+#     posts = Post.objects.all().order_by('-pk')
     
     
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
 
-# urls에서 views.single_post_page로 이동했음.
-# request와 pk를 보내야 함
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post, 
-        }
-    )
-# 들어온 pk의 값들을 가져오는 것임
+# # urls에서 views.single_post_page로 이동했음.
+# # request와 pk를 보내야 함
+#def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#    
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post, 
+#        }
+#     )
+# # 들어온 pk의 값들을 가져오는 것임
 
-# Create your views here.
+# # Create your views here.
 
